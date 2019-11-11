513d0c142109fc69a3a3b649bfdc835755e2324d
NaKia Whitby <whitby.nakia@gmail.com>
2019-11-11 19:09:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addItem()
 });
+
+function addItem() {
+  document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    let item = document.getElementById('new-task-description');
+    let li = document.createElement('li');
+    li.innerHTML = item.value;
+    document.getElementById('list').appendChild(li);
+    e.target.reset();
+  })
+}
+© 2019 GitHub, Inc.
